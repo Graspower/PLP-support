da89d0a11338af377ac66067f799aa983cf0f458
Graspower <162355305+Graspower@users.noreply.github.com>
2025-07-11 15:10:53 +0300

    Update date.py

--- date.py
+++ date.py
@@ -10,8 +10,8 @@ file_list = [
     "List of Tickets advanced.csv"
 ]
 
-start_date = pd.to_datetime("2025-04-04")
-end_date = pd.to_datetime("2025-04-12")
+start_date = pd.to_datetime("2025-07-05")
+end_date = pd.to_datetime("2025-07-11")
 
 # Prepare date range
 all_dates = pd.date_range(start=start_date, end=end_date)
